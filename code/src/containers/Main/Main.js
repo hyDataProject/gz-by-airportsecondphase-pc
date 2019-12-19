620249789c6f2f0e657731ac20091aa5f6352a94
wanjikun <wanjikun@hiynn.com>
2019-12-19 15:04:55 +0800

    登录页

--- code/src/containers/Main/Main.js
+++ code/src/containers/Main/Main.js
@@ -14,74 +14,45 @@ import "./Main.scss";
 export default class Main extends Component {
     constructor(props, context) {
         super(props);
-        this.state={
-            isLogin:true
-        }
-        this.saveToken = this.saveToken.bind(this);
     }
-
 
     componentWillMount() {
-        // this.saveToken();
-        // this.judgeIfTocken();
-        // this.props.history.listen(() => {
-        //     console.log('.....');
-            
-        //     this.judgeIfTocken();
-        // })
-    }
-    //存储token
-    saveToken(){
-        // axios({ //获取token
-        //     method: 'get',
-        //     url: realAddressUrlOne + '/screen/arrivalTotalAndDelayRate',
-        // }).then((result) => {
-        // });
+        this.judgeIfTocken();
+        this.props.history.listen(() => {
+            this.judgeIfTocken();
+        })
     }
 
     //监听路由变化没有token跳转登录页
     judgeIfTocken=()=>{
         let path = this.props.match.path;
-        let token = localStorage.getItem('token')
-        if (!token && path !== '/') {
-            this.props.history.push('/')
+        let token = sessionStorage.getItem('token');
+        console.log('token',token);
+        
+        if (!token) {
+            this.props.history.push('/login')
         }
         
     }
 
-    loginFun=(userCode, password)=>{
-        
-        this.setState({
-            isLogin:true
-        })
-    }
 
     render() {
-        let {isLogin} = this.state;
         return (
             <div className={"Layer"}>
                 {/*默认加载判断路由*/}
-                {
-                    isLogin ? (
-                        <div className="loggedContainer">
-                            <div className="header">
-                                <Header></Header>
-                            </div>
-                            <div className="container">
-                                <Switch>
-                                    <Route path="/main/flyCont" component={FlyCont}/>
-                                    <Route path="/main/terminalCont" component={TerminalCont}/>
-                                    <Route path="/main/publicArea" component={PublicArea}/>
-                                    <Route path="/" component={FlyCont}/>
-                                </Switch>
-                            </div>
-                        </div>
-                    ) : (
-                        <div className="loginCont">
-                            <Login loginFun={this.loginFun}></Login>
-                        </div>
-                    )
-                }
+                <div className="loggedContainer">
+                    <div className="header">
+                        <Header></Header>
+                    </div>
+                    <div className="container">
+                        <Switch>
+                            <Route path="/main/flyCont" component={FlyCont}/>
+                            <Route path="/main/terminalCont" component={TerminalCont}/>
+                            <Route path="/main/publicArea" component={PublicArea}/>
+                            <Route path="/" component={FlyCont}/>
+                        </Switch>
+                    </div>
+                </div>
             </div>
 
         )
